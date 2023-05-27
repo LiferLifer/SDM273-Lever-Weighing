1b5b1e5326daba0f79438b903be23259c8c5c62b
Lifer <lifertomorrow@gmail.com>
2023-05-27 16:26:30 +0800

    v1.1.2
    更换了使用的主要传感器……
    添加了额外的计算芯片。

--- main_control/main_control.ino
+++ main_control/main_control.ino
@@ -2,28 +2,27 @@
  * @file       main_control
  * @brief      The main control program of the lever weighing device realized by the linear motor module.
  * @author     Author
- * @version    Version 1.0.4
- * @date       2023-05-12(Origin)
+ * @version    Version 1.1.2
+ * @date       2023-05-12
  **************************************************************/
 
 /***************************************************************
  * @brief      连线方法
- * @note       MPU6050-UNO: VCC-VCC // GND-GND // SCL-A5 // SDA-A4 // INT-2 (Optional)
- * @note       OLED-UNO: VCC-VCC // GND-GND // SCL-无名port // SDA-无名port
- * @note       HPD970-UNO: PUL- --> 11 // DIR- --> 10 // EN- --> GND // 各正极 --> 5V
+ * @note       GY25-UNO: VCC-VCC // GND-GND // TX-D8 // RX-D9
+ * @note       OLED-UNO: VCC-VCC // GND-GND // SCL-A5 // SDA-A4
+ * @note       HPD970-UNO: PUL- -> 11 // DIR- -> 10 // EN- 可不接 // 各正极 -> 5V
  **************************************************************/
 
-// 引入驱动MPU6050所需的库
+// 引入所需的库
 #include <Kalman.h>
 #include <Wire.h>
 #include <Math.h>
+#include <Stepper.h>
+#include <SoftwareSerial.h>
 
 // // 引入驱动OLED所需的库
 // #include <Adafruit_GFX.h>
 // #include <Adafruit_SSD1306.h>
-
-// 步进电机控制库
-#include <Stepper.h>
 
 // // 定义OLED阵列长宽
 // #define SCREEN_WIDTH 128   // 设置OLED宽度,单位:像素
@@ -37,35 +36,32 @@
 const int PUL = 11;  // 定义脉冲信号引脚
 const int DIR = 10;  // 定义方向信号引脚
 
-// MPU6050相关定义
-const int MPU = 0x68;  // MPU-6050的I2C地址
-const int nValCnt = 7;  // 一次读取寄存器的数量
-const float fRad2Deg = 57.295779513f;  // 将弧度转为角度的乘数
-
-const int nCalibTimes = 1000;  // 校准时读数的次数
-int calibData[nValCnt];  // 初始校准数据
-
-unsigned long nLastTime = 0;  // 上一次读数的时间
-Kalman kalmanPitch;  // Pitch角滤波器
-
-float pitch;   // Pitch角
+// GY25相关定义
+SoftwareSerial GY(8, 9);  // RX为D8，TX为D9
+int YPR[3];
+unsigned char Re_buf[8], counter = 0;
+unsigned char sign = 0;
 
 // 步进电机相关定义
-#define STEPS 200  // 转一周需要的步数（脉冲数）
+#define STEPS 200
 Stepper stepper(STEPS, DIR, PUL);
-
 float initPos = 0;  // 初始步进量
 float lastPos = 0;  // 上一次步进量
-float nowPos = 0;  // 当前步进量
-float diff = 0;
+float nowPos = 0;   // 当前步进量
 
 //float stepAngle = 1.8;  // 步距角
-const float microStep = 200;  // 每 x 个脉冲转一圈
-const float pitchSt = 5;  // 螺距 in mm 记得查看是几头丝杆！
+const float microStep = 200;  // 细分数
+const float pitchSt = 5;      // 螺距 in mm 记得查看是几头丝杆！
 
-// 其他变量
+float lastlastlastPitch = 0;  // 上上上次
+float lastlastPitch = 0;      // 上上次
+float lastPitch = 0;
+float pitch = 0;
+
 float mass = 0;
-const float K = 0;  // 测算得到的比例系数! 
+int key_1 = 1;
+int key_2 = 1;
+float pos[2];
 
 
 /***************************************************************
@@ -74,28 +70,26 @@ const float K = 0;  // 测算得到的比例系数!
   *  @note      none
  **************************************************************/
 void setup() {
-    Serial.begin(9600);   // 初始化串口，指定波特率
-    Wire.begin();         // 初始化Wire库
+  Serial.begin(115200);
+  Serial.println("!!!!!Start Up!!!!!");
 
-    // 初始化MPU6050
-    WriteMPUReg(0x6B, 0);   // 启动MPU6050设备
-    Calibration();          // 执行校准
-    nLastTime = micros();   // 记录当前时间
+  GY.begin(115200);
+  Wire.begin();
 
-    // // 初始化OLED并设置其I2C地址
-    // display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  // 初始化GY25
+  delay(2000);
 
-    // 初始化步进电机驱动器
-    pinMode(PUL, OUTPUT);
-    pinMode(DIR, OUTPUT);
-    stepper.setSpeed(600);  // 每分钟电机转动步数，正整数
+  // // 初始化OLED并设置其I2C地址
+  // display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 
-    // // 装置平衡初始化
-    // words_display("Initializing", "Please wait...");
-    // levelInit();
-    // stepper.step(-100);
+  // 初始化步进电机驱动器
+  pinMode(PUL, OUTPUT);
+  pinMode(DIR, OUTPUT);
+  stepper.setSpeed(500);
 
-    Serial.println("Initialize!");
+  // 装置初始化
+  stepper.step(-300000);
+  Serial.println("!!!!!Finish!!!!!");
 }
 
 
@@ -105,263 +99,92 @@ void setup() {
   *  @note      none
  **************************************************************/
 void loop() {
-    // 控制步进电机
-    level();
 
-    // // 展示当前状态
-    // normal_display(pitch, mass);
-
-    delay(500);
-    Serial.println(">>>>>>>>>>>>>>>>>>>>>>>>zzx>>>>>>>>>>>>>>>>>>>>>>>");
-}
-
-
-/***************************************************************
-  *  @brief     MPU6050 读取俯仰角
-  *  @param     none
-  *  @note      读取 pitch 角，并进行卡尔曼滤波
- **************************************************************/
-float getPitch() {
-    int readouts[nValCnt];
-    ReadAccGyr(readouts);   // 读出测量值
-
-    float realVals[nValCnt];  // update: 7 to nValCnt
-    Rectify(readouts, realVals);   // 根据校准的偏移量进行纠正
-
-    //计算加速度向量的模长，均以g为单位
-    float fNorm = sqrt(realVals[0] * realVals[0] + realVals[1] * realVals[1] + realVals[2] * realVals[2]);
-    float fPitch = getRawPitch(realVals, fNorm);   // 计算Pitch角
-
-    if (realVals[0] < 0) {
-      fPitch = -fPitch;
+  while (key_1 > 0 || key_2 > 0) {
+    while (GY.available()) {
+      Re_buf[counter] = (unsigned char)GY.read();
+      if (counter == 0 && Re_buf[0] != 0xAA) return;  // 检查帧头
+      counter++;
+      if (counter == 8) {
+        counter = 0;  // 重新赋值，准备下一帧数据的接收
+        sign = 1;
+      }
     }
 
-    // Serial.print("InitPitch:");
-    // Serial.println(fPitch);
+    // Serial.println(sign);
+    if (sign) {
+      Serial.println("****************wzy******************");
+      sign = 0;
+      if (Re_buf[0] == 0xAA && Re_buf[7] == 0x55) {  // 检查帧头，帧尾
+        YPR[0] = (Re_buf[1] << 8 | Re_buf[2]);
+        YPR[1] = (Re_buf[3] << 8 | Re_buf[4]);
+        YPR[2] = (Re_buf[5] << 8 | Re_buf[6]);
 
-    // 计算两次测量的时间间隔dt，以秒为单位
-    unsigned long nCurTime = micros();
-    float dt = (double) (nCurTime - nLastTime) / 1000000.0;
-    nLastTime = nCurTime;
-
-    // 对Pitch角进行卡尔曼滤波
-    float fNewPitch = kalmanPitch.getAngle(fPitch, realVals[5], dt);
-
-    // 向串口打印输出Pitch角，运行时在Arduino的串口监视器中查看
-    // Serial.print("Pitch:");
-    // Serial.println(fNewPitch);
-
-    return fNewPitch;
-}
-
-
-/***************************************************************
-  *  @brief     MPU6050 寄存器写入
-  *  @param     nReg 指定的寄存器写入地址
-  *  @param     nVal 要写入的一个字节的值
-  *  @note      向 MPU6050 写入一个字节的数据
- **************************************************************/
-void WriteMPUReg(int nReg, unsigned char nVal) {
-    Wire.beginTransmission(MPU);
-    Wire.write(nReg);
-    Wire.write(nVal);
-    Wire.endTransmission(true);
-}
-
-
-/***************************************************************
-  *  @brief     MPU6050 寄存器读出
-  *  @param     nReg 指定的寄存器读出地址
-  *  @note      从 MPU6050 读出一个字节的数据，返回读出的值
- **************************************************************/
-unsigned char ReadMPUReg(int nReg) {
-    Wire.beginTransmission(MPU);
-    Wire.write(nReg);
-    Wire.requestFrom(MPU, 1, true);
-    Wire.endTransmission(true);
-    return Wire.read();
-}
-
-
-/***************************************************************
-  *  @brief     MPU6050 传感器读数
-  *  @param     *pVals 指定的数组地址
-  *  @note      读出加速度计三个分量、温度和三个角速度，保存在指定数组
- **************************************************************/
-void ReadAccGyr(int *pVals) {
-    Wire.beginTransmission(MPU);
-    Wire.write(0x3B);
-    Wire.requestFrom(MPU, nValCnt * 2, true);
-    Wire.endTransmission(true);
-    for (long i = 0; i < nValCnt; ++i) {
-        pVals[i] = Wire.read() << 8 | Wire.read();
+        Serial.print("YPR:\t");
+        Serial.print(YPR[0], DEC);
+        Serial.print("\t");  // 显示航向
+        Serial.print(YPR[1], DEC);
+        Serial.print("\t");           // 显示俯仰角
+        Serial.println(YPR[2], DEC);  // 显示横滚角
+      }
     }
-}
 
-
-/***************************************************************
-  *  @brief     初始化校准偏移
-  *  @param     none
-  *  @note      对大量读数进行统计，校准平均偏移量
- **************************************************************/
-void Calibration() {
-    float valSums[7] = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0};
-    // 先求和
-    for (int i = 0; i < nCalibTimes; ++i) {
-        int mpuVals[nValCnt];
-        ReadAccGyr(mpuVals);
-        for (int j = 0; j < nValCnt; ++j) {
-            valSums[j] += mpuVals[j];
-        }
-    }
-    // 再求平均
-    for (int i = 0; i < nValCnt; ++i) {
-        calibData[i] = int(valSums[i] / nCalibTimes);
-    }
-    // 设芯片Z轴竖直向下，设定静态工作点。
-    calibData[2] += 16384;
-}
-
-/***************************************************************
-  *  @brief     MPU6050 获取 Pitch 角
-  *  @param     *pRealVals 校准后的测量值
-  *  @param     fNorm 加速度向量的模长
-  *  @note      返回计算得出 Pitch 角
- **************************************************************/
-float getRawPitch(float *pRealVals, float fNorm) {
-    float fNormYZ = sqrt(pRealVals[1] * pRealVals[1] + pRealVals[2] * pRealVals[2]);
-    float fCos = fNormYZ / fNorm;
-    return acos(fCos) * fRad2Deg;
-}
-
-
-/***************************************************************
-  *  @brief     MPU6050 读数纠正消偏
-  *  @param     *pReadout 指定的读入数组地址
-  *  @param     *pRealVals 指定的校准数组地址
-  *  @note      对读数进行纠正，消除偏移，并转换为物理量
- **************************************************************/
-void Rectify(int *pReadout, float *pRealVals) {
-    for (int i = 0; i < 3; ++i) {
-        pRealVals[i] = (float) (pReadout[i] - calibData[i]) / 16384.0f;
-    }
-    pRealVals[3] = pReadout[3] / 340.0f + 36.53;
-    for (int i = 4; i < 7; ++i) {
-        pRealVals[i] = (float) (pReadout[i] - calibData[i]) / 131.0f;
-    }
-}
-
-
-/***************************************************************
-  *  @brief     平衡初始化
-  *  @param     none
-  *  @note      none
- **************************************************************/
-// void levelInit() {
-//     float d = 0;
-//     mass = 0;
-//     do {
-//         pitch = getPitch();
-//         stepper.step(-30);
-//         d += 30;
-//     } while(pitch < 0.5 && pitch > -0.5);
-//     normal_display(pitch, mass);
-//     initPos = d;
-// }
-
-
-/***************************************************************
-  *  @brief     试图平衡
-  *  @param     none
-  *  @note      none
- **************************************************************/
-void level() {
     float d = 0;
     lastPos = nowPos;
-    Serial.println("--------------level!---------------");
-    // pitch = getPitch();
-    // stepper.step(2048);
 
-    while(1) {
-        Serial.println("--------------level while() in!---------------");
-        pitch = getPitch();
-        if(pitch > 0.5) {
-            stepper.step(500);
-            Serial.println("+++++++++++++++++++++++");
-            d += 500 / microStep * pitchSt;
-        } else if(pitch < -0.5) {
-            stepper.step(-500);
-            Serial.println("----------jxy----------");
-            d -= 500 / microStep * pitchSt;
-        }
-        delay(10);
-    }
-
-    Serial.println("pitch: ");
+    pitch = YPR[1];
+    Serial.print("pitch: ");
     Serial.println(pitch);
 
-//     do {
-//       // Serial.println("wzy-----------------------------------------");
-//         if(pitch > 0.5) {
-//           Serial.println("wzy-----------------------------------------");
-//             // advance(5, 1);
-//             stepper.step(-10);
-//             // d += 5;
-//         } else if(pitch < -0.5) {
-//             // advance(5, 0);
-//             Serial.println("jxy$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-//             // d -= 5;
-//         }
-//         pitch = getPitch();
-// //        Serial.print('0');
-//     } while (pitch < 0.5 && pitch > -0.5);
+    if (pitch > 90) {
+      stepper.step(-100);
+      Serial.println("------");
+      d -= 100 / microStep * pitchSt;
+    } else if (pitch < -90) {
+      stepper.step(+100);
+      Serial.println("++++++");
+      d += 100 / microStep * pitchSt;
+    }
 
     nowPos = lastPos + d;
-    // diff = nowPos - initPos;
-    // mass = getMass(diff);
-    // normal_display(pitch, mass);
+
+    Serial.print("nowPos: ");
+    Serial.println(nowPos);
+
+    if (pitch > 0 && lastPitch > 0 && lastlastPitch < 0 && lastlastlastPitch < 0) {
+      key_1--;
+      pos[0] = nowPos;
+    } else if (pitch < 0 && lastPitch < 0 && lastlastPitch > 0 && lastlastlastPitch > 0) {
+      key_2--;
+      pos[1] = nowPos;
+    }
+
+    lastlastlastPitch = lastlastPitch;
+    lastlastPitch = lastPitch;
+    lastPitch = pitch;
+
+    delay(10);
+    // Serial.println(">>>>>>>>>>>>>>>>>>>>>>>>zzx>>>>>>>>>>>>>>>>>>>>>>>");
+  }
+
+  mass = getMass(pos[0], pos[1]);
+
+  // // 展示当前状态
+  // normal_display(pitch, mass);
+
+  Serial.println(pos[0]);
+  Serial.println(pos[1]);
+  Serial.println((pos[1] + pos[0]) / 2);
+  Serial.println(mass);
+
+  Serial.println(">>>>>>>>>>>>>>>>>>>>>>>>zzx>>>>>>>>>>>>>>>>>>>>>>>");
 }
 
 
- /***************************************************************
-   *  @brief     获取当前质量
-   *  @param     diff: 位移差
-   *  @note      none
-  **************************************************************/
-// float getMass(float diff) {
-//     float nowMass = diff * K;
-//     return nowMass;
-// }
-
-
-/***************************************************************
-  *  @brief     OLED 一般调用
-  *  @param     pitch: the pitch you want to show
-  *  @param     mass: the mass you want to show
-  *  @note      使用 OLED 显示计算得出的 pitch 以及 weight
- **************************************************************/
-// void normal_display(float pitch, float mass) {
-//     // 清除屏幕
-//     display.clearDisplay();
-
-//     // 设置字体颜色,白色可见
-//     display.setTextColor(WHITE);
-
-//     // 设置字体大小
-//     display.setTextSize(3);
-
-//     // 设置光标位置
-//     display.setCursor(0, 20);
-
-//     // 显示pitch
-//     display.print("Pitch:   ");
-//     display.print(pitch);
-
-//     // 显示weight
-//     display.setCursor(0, 45);
-//     display.print("Mass:    ");
-//     display.print(mass);
-
-//     // 显示字符
-//     display.display();
-// }
+float getMass(float pos0, float pos1) {
+  float x;
+  x = -(pos0 + pos1) / 2;
+  Serial.println(x);
+  mass = -1.3225 * x + 1215;
+  return mass;
+}
