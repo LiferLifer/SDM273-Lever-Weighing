022065f95a257f9ac39e4c2ed57836dc7c322335
Lifer <lifertomorrow@gmail.com>
2023-06-03 19:57:59 +0800

    v1.2.0
    现场送进120！

--- main_control/main_control.ino
+++ main_control/main_control.ino
@@ -2,14 +2,14 @@
  * @file       main_control
  * @brief      The main control program of the lever weighing device realized by the linear motor module.
  * @author     Author
- * @version    Version 1.1.3
+ * @version    Version 1.2.0
  * @date       2023-05-12
  **************************************************************/
 
 /***************************************************************
  * @brief      连线方法
- * @note       GY25-UNO: VCC-VCC // GND-GND // TX-D8 // RX-D9
- * @note       OLED-UNO: VCC-VCC // GND-GND // SCL-A5 // SDA-A4
+ * @note       MPU6050-UNO: VCC-VCC // GND-GND // SCL-A5 // SDA-A4 // INT-2 (Optional)
+ * @note       OLED-UNO: VCC-VCC // GND-GND // SCL-无名port // SDA-无名port
  * @note       HPD970-UNO: PUL- -> 11 // DIR- -> 10 // EN- 可不接 // 各正极 -> 5V
  **************************************************************/
 
@@ -17,6 +17,7 @@
 #include <Kalman.h>
 #include <Wire.h>
 #include <Math.h>
+#include <string.h>
 #include <Stepper.h>
 #include <SoftwareSerial.h>
 
@@ -32,12 +33,21 @@
 // #define OLED_RESET 4
 // Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+// LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 // 定义步进电机驱动器引脚
 const int PUL = 11;  // 定义脉冲信号引脚
 const int DIR = 10;  // 定义方向信号引脚
 
-// GY25相关定义
-SoftwareSerial GY(8, 9);  // 虚拟串口，RX为D8，TX为D9
+float lastlastlastPitch = 0;  // 上上上次
+float lastlastPitch = 0;      // 上上次
+float lastPitch = 0;
+float pitch = 0;  // Pitch角
+
+// SoftwareSerial Voice(6, 7);  // 虚拟串口，RX为D6，TX为D7
+
+// GY25
+SoftwareSerial GY(8, 9);  // RX为D8，TX为D9
 int YPR[3];
 unsigned char Re_buf[8], counter = 0;
 unsigned char sign = 0;
@@ -45,23 +55,17 @@ unsigned char sign = 0;
 // 步进电机相关定义
 #define STEPS 200
 Stepper stepper(STEPS, DIR, PUL);
-float initPos = 0;  // 初始步进量
+// float initPos = 0;  // 初始步进量
 float lastPos = 0;  // 上一次步进量
 float nowPos = 0;   // 当前步进量
 
-//float stepAngle = 1.8;  // 步距角
-const float microStep = 200;  // 细分数
-const float pitchSt = 5;      // 螺距 in mm 记得查看是几头丝杆！
-
-float lastlastlastPitch = 0;  // 上上上次
-float lastlastPitch = 0;      // 上上次
-float lastPitch = 0;
-float pitch = 0;
-
+// 其他变量
 float mass = 0;
 int key_1 = 1;
 int key_2 = 1;
+int keeeey = 1;
 float pos[2];
+String str;
 
 
 /***************************************************************
@@ -70,8 +74,9 @@ float pos[2];
   *  @note      none
  **************************************************************/
 void setup() {
-  Serial.begin(115200);
-  Serial.println("!!!!!Start Up!!!!!");
+  Serial.begin(9600);
+  // Serial.println("!!!!!Start Up!!!!!");
+  Serial.print("233");
 
   GY.begin(115200);
   Wire.begin();
@@ -80,14 +85,18 @@ void setup() {
   // // 初始化OLED并设置其I2C地址
   // display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 
+  // lcd.init();
+  // lcd.backlight();
+
   // 初始化步进电机驱动器
   pinMode(PUL, OUTPUT);
   pinMode(DIR, OUTPUT);
-  stepper.setSpeed(500);
+  stepper.setSpeed(2000);
 
-  // 装置初始化
+  // // 装置平衡初始化
   stepper.step(-300000);
-  Serial.println("!!!!!Finish!!!!!");
+  stepper.setSpeed(700);
+  // Serial.println("!!!!!Finish!!!!!");
 }
 
 
@@ -97,57 +106,60 @@ void setup() {
   *  @note      none
  **************************************************************/
 void loop() {
-
+  // disp(mass);
   while (key_1 > 0 || key_2 > 0) {
     while (GY.available()) {
       Re_buf[counter] = (unsigned char)GY.read();
       if (counter == 0 && Re_buf[0] != 0xAA) return;  // 检查帧头
       counter++;
-      if (counter == 8) {
-        counter = 0;  // 重新赋值，准备下一帧数据的接收
+      if (counter == 8)  //接收到数据
+      {
+        counter = 0;  //重新赋值，准备下一帧数据的接收
         sign = 1;
       }
     }
 
+    // 控制步进电机
     // Serial.println(sign);
     if (sign) {
-      Serial.println("****************wzy******************");
+      // Serial.println("****************wzy******************");
       sign = 0;
       if (Re_buf[0] == 0xAA && Re_buf[7] == 0x55) {  // 检查帧头，帧尾
         YPR[0] = (Re_buf[1] << 8 | Re_buf[2]);
         YPR[1] = (Re_buf[3] << 8 | Re_buf[4]);
         YPR[2] = (Re_buf[5] << 8 | Re_buf[6]);
 
-        Serial.print("YPR:\t");
-        Serial.print(YPR[0], DEC);
-        Serial.print("\t");  // 显示航向
-        Serial.print(YPR[1], DEC);
-        Serial.print("\t");           // 显示俯仰角
-        Serial.println(YPR[2], DEC);  // 显示横滚角
+        // Serial.print("YPR:\t");
+        // Serial.print(YPR[0], DEC);
+        // Serial.print("\t");  // 显示航向
+        // Serial.print(YPR[1], DEC);
+        // Serial.print("\t");           // 显示俯仰角
+        // Serial.println(YPR[2], DEC);  // 显示横滚角
       }
     }
 
     float d = 0;
     lastPos = nowPos;
 
+    // Serial.println("--------------level!---------------");
     pitch = YPR[1];
-    Serial.print("pitch: ");
-    Serial.println(pitch);
+    // Serial.print("pitch: ");
+    // Serial.println(pitch);
 
     if (pitch > 90) {
       stepper.step(-100);
       Serial.println("------");
-      d -= 100 / microStep * pitchSt;
+      d -= 2.5;
     } else if (pitch < -90) {
       stepper.step(+100);
       Serial.println("++++++");
-      d += 100 / microStep * pitchSt;
+      d += 2.5;
     }
 
     nowPos = lastPos + d;
+    // Serial.print("nowPos: ");
+    // Serial.println(nowPos);
 
-    Serial.print("nowPos: ");
-    Serial.println(nowPos);
 
     if (pitch > 0 && lastPitch > 0 && lastlastPitch < 0 && lastlastlastPitch < 0) {
       key_1--;
@@ -162,36 +174,55 @@ void loop() {
     lastPitch = pitch;
 
     delay(10);
-    // Serial.println(">>>>>>>>>>>>>>>>>>>>>>>>zzx>>>>>>>>>>>>>>>>>>>>>>>");
   }
 
   mass = getMass(pos[0], pos[1]);
 
-  // // 展示当前状态
-  // normal_display(pitch, mass);
+  // str = String(mass, 2);
+  // Serial.print(str);
+  delay(1500);
 
-  Serial.println(pos[0]);
-  Serial.println(pos[1]);
-  Serial.println((pos[1] + pos[0]) / 2);
-  Serial.println(mass);
+  if (keeeey == 1) {
+    // Serial.print("您已经重达");
+    mass = getMass(pos[0], pos[1]);
+    str = String(mass, 2);
+    Serial.print(str);
+    // Serial.print("克辣");
+    delay(3000);
+    keeeey--;
+  }
 
-  Serial.println(">>>>>>>>>>>>>>>>>>>>>>>>zzx>>>>>>>>>>>>>>>>>>>>>>>");
+  // Serial.println(pos[0]);
+  // Serial.println(pos[1]);
+  // Serial.print("Pos:  ");
+  // Serial.println((pos[1] + pos[0]) / 2);
+  // Serial.print("mass: ");
+  // Serial.println(mass);
+  // disp(mass);
+
+  // Serial.println(">>>>>>>>>>>>>>>>>>>>>>>>zzx>>>>>>>>>>>>>>>>>>>>>>>");
 }
 
-
- /***************************************************************
-   *  @brief     获取当前质量
-   *  @param     pos0: 初次翻转位置
-   *  @param     pos1: 再次翻转位置
-   *  @note      none
-  **************************************************************/
 float getMass(float pos0, float pos1) {
   float x;
   x = -(pos0 + pos1) / 2;
-  Serial.println(x);
-  mass = -1.3225 * x + 1215;
+  mass = -1.4097 * x + 1187.7;
   return mass;
 }
+
+
+/***************************************************************
+  *  @brief     LCD 调用
+  *  @param     mass: the mass you want to show
+  *  @note      使用 LCD 显示信息
+ **************************************************************/
+// void disp(float mass) {
+//   lcd.setCursor(0, 0);
+//   lcd.print("SDM273");
+//   lcd.setCursor(0, 1);
+//   lcd.print("Mass: ");
+//   lcd.print(mass);
+// }
 
 
 /***************************************************************
@@ -207,7 +238,7 @@ float getMass(float pos0, float pos1) {
 //     display.setTextColor(WHITE);
 
 //     // 设置字体大小
-//     display.setTextSize(4);
+//     display.setTextSize(1.5);
 
 //     // 设置光标位置
 //     display.setCursor(0, 20);
